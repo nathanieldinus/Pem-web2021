365eb67f2ea997ba1c015060925c346ffe86ea4e
nathanieldinus <tvpeace7@gmail.com>
2022-01-20 14:42:29 +0700

    Tokoku - Tugas Akhir UAS

---
+++ UAS-Tokoku/assets/js/jsproduct.js
@@ -0,0 +1,9 @@
+function on() {
+    var menunBar = document.getElementById("menuBar");
+  
+    var overlay = document.getElementById("overlay");
+  
+    menunBar.classList.toggle("menuBarStyle");
+    overlay.classList.toggle("overlayStyle");
+  }
+  
